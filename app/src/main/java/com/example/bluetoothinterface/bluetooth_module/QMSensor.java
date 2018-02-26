115bd61510f70157184ceffc6936451b00c27cde
prashant45 <prashant123219@gmail.com>
2018-02-26 11:56:03 +0100

    FramesLost quality check on 500 frames every 5 seconds

--- app/src/main/java/com/example/bluetoothinterface/bluetooth_module/QMSensor.java
+++ app/src/main/java/com/example/bluetoothinterface/bluetooth_module/QMSensor.java
@@ -2,8 +2,8 @@ package com.example.bluetoothinterface.bluetooth_module;
 
 import com.example.bluetoothinterface.interfaces.IQMSensor;
 
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created by jalil on 2/6/2018.
@@ -11,28 +11,98 @@ import java.util.List;
 
 class QMSensor implements IQMSensor {
 
-    public int lostFrames(List<DataFrameFactory> last5SecondsData) {
-        int startCount;
-        int endCount;
-        int lostFrames;
-        int size = last5SecondsData.size();
+    private ArrayList<DataFrameFactory> buffer = new ArrayList<>();
 
-        startCount = last5SecondsData.get(0).getCount();
-        endCount = last5SecondsData.get(size - 1).getCount(); // because indexing is from 0
-        lostFrames = endCount - (startCount + size - 1);
+    public int lostFrames(ArrayList<DataFrameFactory> last5SecondsData) {
 
-        System.out.println("Size : " + size + "   Start Count :" + startCount + "  End Count :" + endCount + "  Frame Loss " + lostFrames + "\n\n\n");
+        ArrayList<DataFrameFactory> localData = new ArrayList<>();
+        int framesLost = 0;
 
-        return lostFrames;
+        int numberOfFramesToProcess = 500; // 0 to 499 index
+        int bufferSize = buffer.size();
+        int incomingDataSize = last5SecondsData.size();
+        int availableDataSize = bufferSize + incomingDataSize;
+//        System.out.println("Initial Buffer Size: " + bufferSize);
+
+//        System.out.println("Incoming Size: " + incomingDataSize + "\tEnd Count: " + last5SecondsData.get(incomingDataSize - 1).getCount());
+//        System.out.println("Available Data Size : " + availableDataSize);
+
+        // Check if previously there was any buffer stored
+        if (bufferSize != 0) {
+            localData.addAll(buffer);
+            buffer.clear();
+        }
+
+        // Add only numberOfFramesToProcess limit in localData
+        int numberOfFramesLimitIndex = numberOfFramesToProcess - bufferSize - 1;
+//        System.out.println("Last possible index for Frames limit in local Data " + numberOfFramesLimitIndex);
+        try {
+            for (int i = 0; i < numberOfFramesLimitIndex; ++i) {
+                localData.add(last5SecondsData.get(i));
+            }
+            //TODO: Do the processing and throw Exception if necessary
+            int startCount = localData.get(0).getCount();
+            int endCount = localData.get(localData.size() - 1).getCount();
+
+            framesLost = endCount - startCount - numberOfFramesToProcess ;
+//            System.out.println("Available Data Size was : " + availableDataSize);
+//            System.out.println("LocalData : \tStart Count: " + localData.get(0).getCount() + " \tEnd Count: " + localData.get(localData.size() - 1).getCount());
+            System.out.println("Every 5 seconds lostFrames is " + framesLost);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // If there are more than numberOfFramesToProcess in 5 second data, then store the rest in buffer for next processing loop
+        try {
+            if (availableDataSize > numberOfFramesToProcess) {
+                for (int i = numberOfFramesLimitIndex + 1; i < incomingDataSize; ++i) {
+                    buffer.add(last5SecondsData.get(i));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        System.out.println("5 second data size : " + size + "\n buffer size : " + bufferSize + "\n Local Data size : " + localData.size() + "\n\n");
+
+
+//        int lostFrames = 0;
+//
+//        for (int i = 0; i < 500; i+=20) {
+//
+//
+//            int localStartCount = localData.get(i).getCount();
+//            int localEndCount = localData.get(i + 100).getCount();
+//
+//            int localLostFrames = localEndCount - (localStartCount + 100 - 1);
+//
+//            lostFrames += localLostFrames;
+//
+//        }
+//        int startCount;
+//        int endCount;
+//
+//
+//        startCount = localData.get(0).getCount();
+//        endCount = localData.get(size - 1).getCount(); // because indexing is from 0
+//        lostFrames = endCount - (startCount + size - 1);
+//
+//        for (int i = 0; i < size; ++i) {
+//            int count = localData.get(i).getCount();
+//            System.out.println(count + "\n");
+//        }
+
+//        System.out.println("Size : " + size + "   Start Count :" + startCount + "  End Count :" + endCount + "  Frame Loss " + lostFrames + "\n\n\n");
+
+        return framesLost;
     }
-
 
     public boolean shouldDisconnect(Date lastReadTime) {
 
         // TODO: Need better logic ?
         // Not fired when sensor was out of range
         Date nowTime = new Date();
-        return (nowTime.getTime() - lastReadTime.getTime())/1000 >= 10;
+        return (nowTime.getTime() - lastReadTime.getTime())/1000 >= 10000;
     }
 
     //TODO: in a row lost packages.
--- app/src/main/java/com/example/bluetoothinterface/interfaces/IQMSensor.java
+++ app/src/main/java/com/example/bluetoothinterface/interfaces/IQMSensor.java
@@ -2,14 +2,14 @@ package com.example.bluetoothinterface.interfaces;
 
 import com.example.bluetoothinterface.bluetooth_module.DataFrameFactory;
 
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 /**
  * Created by jalil on 2/6/2018.
  */
 
 public interface IQMSensor {
-    int lostFrames(List<DataFrameFactory> last5SecondsData);
+    int lostFrames(ArrayList<DataFrameFactory> last5SecondsData);
     boolean shouldDisconnect(Date lastReadTime);
 }
